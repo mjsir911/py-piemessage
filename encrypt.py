cc93384e405714df82401aa1c67104c577748aa5
Marco Sirabella <msirabel@gmail.com>
2016-09-21 12:53:46 -0400

    - Some junky stuff

---
+++ encrypt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+
+__appname__    = ""
+__author__     = "Marco Sirabella"
+__copyright__  = ""
+__credits__    = ["Marco Sirabella"]  # Authors and bug reporters
+__license__    = "GPL"
+__version__    = "1.0"
+__maintainers__ = "Marco Sirabella"
+__email__      = "msirabel@gmail.com"
+__status__     = "Prototype"  # "Prototype", "Development" or "Production"
+__module__     = ""
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
+    def encrypt(self, raw):
+        raw = pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
