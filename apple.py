aeac9ce58c8151f9c4e9ab91b80b038fa51c9385
Marco Sirabella <msirabel@gmail.com>
2016-09-28 08:51:10 -0400

    - Tidied up a bit

--- apple.py
+++ apple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__appname__    = "pymessage"
+import uuid
+import socket
+import time
+
+__appname__    = "pyMessage"
 __author__     = "Marco Sirabella, Owen Davies"
 __copyright__  = ""
 __credits__    = "Marco Sirabella, Owen Davies"
@@ -12,14 +16,10 @@ __email__      = "msirabel@gmail.com, dabmancer@dread.life"
 __status__     = "Prototype"
 __module__     = ""
 
-port = 60000
-chat = 'chat.db'
+chat = '~/Library/Messages/chat.db'
 sqlrecieve = 'select * from message where not is_from_me order by date desc limit 1'
 sqlsender = "select message.guid, chat.chat_identifier from message inner join chat_message_join on message.ROWID = chat_message_join.message_id inner join chat on chat_message_join.chat_id = chat.ROWID where message.guid = '{}'"
-address = ('localhost', 60000)
-
-import socket
-import time
+address = ('sirabella.org', 8000)
 
 def dosql(db, command, arg=None):
     """ Send database sqlite script, with or without arguments for {}"""
@@ -35,12 +35,12 @@ def dosql(db, command, arg=None):
 def connect():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(address)
-    sock.sendall(b"abcd\n\x00")
+    sock.send("{}\n".format(hex(uuid.getnode())).encode() + bytes(True)) #ik this is such BAD CODE
     print("sent")
-    lguid = sock.recv(60);
-    print(lguid)
-    contents = b"some stuff"
-    sock.sendall(contents)
+    lguid = sock.recv(64).decode();
+    print('recieved ' + lguid)
+    contents = "latest guid +5: {}".format(lguid + '5')
+    sock.send(contents.encode())
     sock.close()
 
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__appname__    = "pymessage"
+__appname__    = "pyMessage"
 __author__     = "Marco Sirabella, Owen Davies"
 __copyright__  = ""
 __credits__    = "Marco Sirabella, Owen Davies"
@@ -12,7 +12,7 @@ __email__      = "msirabel@gmail.com, dabmancer@dread.life"
 __status__     = "Prototype"
 __module__     = ""
 
-port = 60000
+port = 8000
 chat = 'chat.db'
 sqlrecieve = 'select * from message where not is_from_me order by date desc limit 1'
 sqlsender = "select message.guid, chat.chat_identifier from message inner join chat_message_join on message.ROWID = chat_message_join.message_id inner join chat on chat_message_join.chat_id = chat.ROWID where message.guid = '{}'"
@@ -35,41 +35,46 @@ def dosql(db, command, arg=None):
 def connect():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('', port))
-    sock.listen(4) #Should be changed to a different number
+    sock.listen(4) #Should be changed to a different number #But does the number DO anything??
     while True:
         print("starting")
         conn, addr = sock.accept()
-        t = threading.Thread(target=stuff, args = (conn,))
+        t = threading.Thread(target=stuff, args = ([conn])) # So apparently this comma is needed # Replaced with tuple to look BETTER
         t.start()
-        print("stopping")
-
-def client(sock, machine):
-    lguid = sock.recv(60)
-    print(lguid)
-    contents = b"some stuff"
-    sock.sendall(contents)
-
-def apple(sock, machine):
-    lguid = b"1234" #call sql later
-    print(None == sock.sendall(lguid))
-    r = sock.recv(100000);
-    print(r)
-    #put r into table.
+        print("Looping")
 
 def stuff(sock):
     print("entered socket")
     handshake = sock.recv(16).decode()
-    print(handshake)
-    machine, flag = handshake.split("\n")
-    print("2")
-    print(flag)
-    print(flag==0)
-    if flag=="\x00":
+    #print('handshake data: ' + handshake)
+    machine, flag = handshake.split("\n") # I question why you named this machine
+    #print("2") # these numbers arent needed anymore probs
+    #print('flag is {}'.format(type(flag)))
+    #print('flag data is {}'.format(flag))
+    print('uuid is {}'.format(machine))
+    print('flag bool is {}'.format(flag == bytes(True).decode()))
+    if flag==bytes(True).decode():
         apple(sock, machine)
     else:
         client(sock, machine)
-    print("3")
+    #print("3")
     sock.close()
-    print("4")
+    #print("4")
+
+def client(sock, machine):
+    print('client connection')
+    lguid = sock.recv(60).decode()
+    print('guid: ' + lguid)
+    contents = "4321".encode()
+    sock.sendall(contents)
+
+def apple(sock, machine):
+    print('apl connection')
+    lguid = "1234" #call sql later
+    print('Any errors?: {}'.format(None == sock.send(lguid.encode())))
+    r = sock.recv(1024).decode();
+    print(r)
+    #put r into table.
+
 
 connect()
